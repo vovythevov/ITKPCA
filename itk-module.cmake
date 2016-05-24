89294b5f23c9e27cc296ab4e1c5271ee3bc144e2
Johan Andruejol <Johan Andruejol>
2016-05-24 12:50:43 -0400

    Move PrincipalComponentsAnalysis to itk-module hierarchy
    This is the first part of a series of commit to convert this repository to
    work with ITK's external modules infrastructure.
    This commit allows to configure/generate the module against an ITK build.
    The itkVectorFieldPCA.* files were moved to the folder named include which
    the external module expects for header files.
    The itk-module.cmake was added to describe the module and its dependencies.

---
+++ itk-module.cmake
@@ -0,0 +1,10 @@
+set(DOCUMENTATION "This module contains an implementation of the 
+principal component analysis for the ITK toolkit.")
+
+itk_module(PrincipalComponentsAnalysis
+  DEPENDS
+    ITKCommon
+  EXCLUDE_FROM_DEFAULT 
+  DESCRIPTION
+    "${DOCUMENTATION}"
+)
